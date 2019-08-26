8e236a59559b9d669ee6bc8ae266e2e3869c3c13
michaelspeed <michael.the.don@gmail.com>
2019-08-26 16:22:35 +0530

    removed unused constants and defined new constants

---
+++ src/globals/Intent.ts
@@ -0,0 +1,8 @@
+export const Intent = {
+    NONE: "none" as "none",
+    PRIMARY: "primary" as "primary",
+    SUCCESS: "success" as "success",
+    WARNING: "warning" as "warning",
+    DANGER: "danger" as "danger",
+    INFO: "info" as "info",
+};
---
+++ src/globals/index.ts
@@ -0,0 +1,3 @@
+export {default as StackingOrder} from './StackingOrder'
+export {default as Intent} from './Intent'
+export {default as Position} from './Position'
